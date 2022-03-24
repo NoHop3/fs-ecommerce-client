ad2420cecd22360fd888228728d75635e488cd4e
NoHop3 <u_sux@abv.bg>
2022-03-25 00:02:38 +0100

    Added middlware to automatically remove expired tokens from backend.

--- src/redux/store/index.tsx
+++ src/redux/store/index.tsx
@@ -1,12 +1,22 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import jwt_decode from "jwt-decode";
 
 import rootReducer from "../reducers";
 
+const tokenAutoRemoval = (store: any) => (next: any) => (action: any) => {
+  const token: any = JSON.parse(localStorage.getItem("token") as any);
+  const decoded = token && (jwt_decode(token) as any).exp;
+  if (token && decoded < Date.now() / 1000) {
+    next(action);
+    localStorage.clear();
+  }
+  next(action);
+};
 
 const store = () => {
-  const middleware = [thunk];
+  const middleware = [thunk, tokenAutoRemoval];
   const reduxStore = createStore(
     rootReducer,
     composeWithDevTools(applyMiddleware(...middleware))
